804f33fb508487a89f80f6423b94067fe08d52a5
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-05-16 00:14:05 -0300

    finalizano o projeto

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,23 @@
 import { Link } from 'react-router-dom';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 import './footer.css';
 
 function Footer() {
+  const dataAtual = new Date();
+  const anoAtual = dataAtual.getFullYear();
+
   return ( 
     <footer>
-      <Link>
-        <span>Linkedin</span>
-      </Link>
-      <Link>
-        <span>Github</span>
-      </Link>
+      <span>Copyright © {anoAtual} Todos direitos reservados por JVG87.</span>
+      <div className="social-medias">
+        <Link to='https://linkedin.com' target='_blank' rel='external'>
+          <FaLinkedin/>
+        </Link>
+        <Link to='https://github.com/jvg87' target='_blank' rel='external'>
+          <FaGithub/>
+        </Link>
+      </div>
     </footer>
   );
 }
